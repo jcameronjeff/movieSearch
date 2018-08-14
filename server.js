f3cdba1cb95ee27e31ddc0e0298ef5c0667b1396
Jeff Cameron <jcameronjeff@gmail.com>
2018-08-14 17:39:55 -0400

    re-deploy

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+app.use('/', express.static(__dirname + '/public'));
 
 app.get('/favorites', function (req, res) {
   var data = fs.readFileSync('./data.json');
